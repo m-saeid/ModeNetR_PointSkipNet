282f8b0f73a7ba00bc77c056d2d3e835fdb2a20c
Mohammad Saeid <89332212+m-saeid@users.noreply.github.com>
2024-09-08 19:18:50 +0330

    Models > pointnet and  pointnet2 and pointNN

---
+++ Models/Point-NN/run_test.sh
@@ -0,0 +1,5 @@
+
+# RUN THE "run_nn_cls.py" DIRECTLY. I'VE CHANGEDDDDDDDDDDDDD IT.
+
+#python run_nn_cls.py --dataset mn40
+#python run_pn_mn40.py --msg pn_6212
---
+++ Models/Pointnet and Poinnet2/run_test.sh
@@ -0,0 +1,6 @@
+# pointnet
+python test_classification.py --log_dir pointnet_cls
+
+# pointnet 2
+#python test_classification.py --log_dir pointnet2_ssg_wo_normals
+#python test_classification.py --log_dir pointnet2_cls_msg_wo_normals
