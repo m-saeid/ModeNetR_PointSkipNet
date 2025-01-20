4963996b80b784102365814df3280cb25c24a4bb
Mohammad Saeid <89332212+m-saeid@users.noreply.github.com>
2025-01-20 06:09:13 -0800

    Delete Point-SkipNet directory

--- Point-SkipNet/run_test.sh
+++
@@ -1,2 +0,0 @@
-# Test Point-SkipNet
-python test_classification.py --log_dir pn2_cls_ssg_new_dset_cat_rot_1
--- Point-SkipNet/run_train.sh
+++
@@ -1,2 +0,0 @@
-# Train Point-SkipNet
-python train_classification.py --model pointnet2_cls_ssg --batch_size 110 --seed 1 --epoch 200 --aug_type rotation --res_type cat # --log_dir pn2_cls_ssg_new_dset_cat_rot_1
