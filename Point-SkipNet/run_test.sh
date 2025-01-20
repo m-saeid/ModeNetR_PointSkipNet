cb7a73e4f60656080a04d150312764908f005c35
Mohammad Saeid <89332212+m-saeid@users.noreply.github.com>
2025-01-20 02:59:43 -0800

    Update run_test.sh

--- Point-SkipNet/run_test.sh
+++ Point-SkipNet/run_test.sh
@@ -1,9 +1,2 @@
-# Point-SkipNet
+# Test Point-SkipNet
 python test_classification.py --log_dir pn2_cls_ssg_new_dset_cat_rot_1
-
-# pointnet
-#python test_classification.py --log_dir pointnet_cls
-
-# pointnet 2
-#python test_classification.py --log_dir pointnet2_ssg_wo_normals
-#python test_classification.py --log_dir pointnet2_cls_msg_wo_normals
