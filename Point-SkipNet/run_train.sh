a855d7d8af8c3c9db9a18fe4476f54055a9f6253
Mohammad Saeid <89332212+m-saeid@users.noreply.github.com>
2025-01-23 10:13:41 -0800

    Update run_train.sh

--- Point-SkipNet/run_train.sh
+++ Point-SkipNet/run_train.sh
@@ -2,10 +2,10 @@
 
 # Check if dataset is modelnetR
 if [ "$dataset" = "modelnetR" ]; then
-    python train_classification.py --model pointskipnet --dataset modelnetR --batch_size 110 --seed 1 --epoch 200 --aug_type rotation --res_type cat
+    python train_classification.py --model pointskipnet --dataset modelnetR --batch_size 16 --seed 1 --epoch 300 --aug_type rotation --res_type cat
 
 elif [ "$dataset" = "modelnet" ]; then
-    python train_classification.py --model pointskipnet --dataset modelnet --batch_size 110 --seed 1 --epoch 200 --aug_type rotation --res_type cat
+    python train_classification.py --model pointskipnet --dataset modelnet --batch_size 16 --seed 1 --epoch 300 --aug_type rotation --res_type cat
 
 else
     echo "Unsupported dataset: $dataset"
